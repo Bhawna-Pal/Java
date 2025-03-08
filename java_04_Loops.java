054e24b44be6d788bf6e5d95be2745a55e7a1c36
Bhawna Pal <bhawnapal2104@gmail.com>
2025-03-08 12:01:21 +0530

    Commit

--- java_04_Loops.java
+++ java_04_Loops.java
@@ -7,148 +7,153 @@ public class java_04_Loops {
 
         int counter = 0;
         while (counter < 10) {
-            // System.out.println("Hello World");
+            System.out.println("Hello World");
             counter++;
         }
-        // System.out.println("Printed Hw 10x");
+        System.out.println("Printed Hw 10x");
 
         // Print Number from 1 To 10
 
         int count = 1;
         while (count <= 10) {
-            // System.out.print(count +" ");
+            System.out.print(count +" ");
             count++;
     }
-    // System.out.println();
+    System.out.println();
 
     // Print Number from 1 To N
 
     Scanner sc = new Scanner(System.in);
-    // int range = sc.nextInt();
-    // int count1 = 1;
-    // while (count1 <= range) {
-        // System.out.print(count1 + " ");
-        // count1++;
-    // }
-    // System.out.println();
+    int range = sc.nextInt();
+    int count1 = 1;
+    while (count1 <= range) {
+        System.out.print(count1 + " ");
+        count1++;
+    }
+    System.out.println();
 
     // Sum of n natural numbers
 
 
-    // int num1 = sc.nextInt();
-    // int sum = 0;
-    // int i1 = 1;
-    // while (i <= num1) {
-        // sum += i1;
-        // i1++;
-    // }
-    // System.out.println("Sum is : " + sum);
+    int num1 = sc.nextInt();
+    int sum = 0;
+    int i1 = 1;
+    while (i1 <= num1) {
+        sum += i1;
+        i1++;
+    }
+    System.out.println("Sum is : " + sum);
 
     //  For Loops
  
-    // for(int i2 = 1; i2 <= 10; i2++) {
-    //     System.out.println("Hello World");
-    // }
+    for(int i2 = 1; i2 <= 10; i2++) {
+        System.out.println("Hello World");
+    }
 
     // Print Square Pattern
 
-    // for (int line = 1; line <= 4; line++) {
-    //     System.out.println("* * * *");
-    // }
+    for (int line = 1; line <= 4; line++) {
+        System.out.println("* * * *");
+    }
 
     // Print Reverse of a number
 
-    // int num2 = 10899;
-    // while (num2 > 0) {
-    //     int lastdigit = num2 % 10;
-    //     System.out.print(lastdigit + " ");
-    //     num2 /= 10;      
-    // }
-    // System.out.println();
+    int num2 = 10899;
+    while (num2 > 0) {
+        int lastdigit = num2 % 10;
+        System.out.print(lastdigit + " ");
+        num2 /= 10;      
+    }
+    System.out.println();
 
     // Reverse the number
 
-    // int num3 = 10899;
-    // int rev = 0;
-    // while (num3 > 0) {
-    //    int  lastDigit = num3 % 10;
-    //    rev = (rev * 10) + lastDigit;
-    //    num3 /= 10;
-    // }
-    // System.out.println(rev);
+    int num3 = 10899;
+    int rev = 0;
+    while (num3 > 0) {
+       int  lastDigit = num3 % 10;
+       rev = (rev * 10) + lastDigit;
+       num3 /= 10;
+    }
+    System.out.println(rev);
 
     //  do - while Loop
 
-    // int count2 = 1;
-    // do {
-    //     System.out.println("Hello World");
-    //     count2++;
-    // } while (count2 <= 10);
+    int count2 = 1;
+    do {
+        System.out.println("Hello World");
+        count2++;
+    } while (count2 <= 10);
 
     //  Break Statement 
 
-    // for (int i3 = 1; i3 < 5; i3++) {
-    //     if(i3 == 3){
-    //     break;
-    //     }
-    //     System.out.println(i3);
-    // }
-    // System.out.println("I am the out of loop");
+    for (int i3 = 1; i3 < 5; i3++) {
+        if(i3 == 3){
+        break;
+        }
+        System.out.println(i3);
+    }
+    System.out.println("I am the out of loop");
 
     // Keep entering numbers till user enter a multiple of 10
 
-    // do{
-    //     System.out.print("Enter your number: ");
-    //     int n = sc.nextInt();
-    //     if (n % 10 == 0) {
-    //         System.out.println("I am out of loop");
-    //         break;
-    //     }
-    //     System.out.println(n);
-    // } while(true);
+    do{
+        System.out.print("Enter your number: ");
+        int n = sc.nextInt();
+        if (n % 10 == 0) {
+            System.out.println("I am out of loop");
+            break;
+        }
+        System.out.println(n);
+    } while(true);
 
 
     //  continue statements
 
-//     for (int i4 = 1; i4 <= 5 ; i4++) {
-//         if (i4 == 3) {
-//             continue;
-//         }
-//         System.out.println(i4);
-//     }
+    for (int i4 = 1; i4 <= 5 ; i4++) {
+        if (i4 == 3) {
+            continue;
+        }
+        System.out.println(i4);
+    }
 
           
-    // //    Display all numbers entered by user except multiples of 10 
+    //    Display all numbers entered by user except multiples of 10 
 
-    // do {
-    //     System.out.print("Enter Your Number : ");
-    //     int n2 = sc.nextInt();
-    //     if (n2 % 10 == 0) {
-    //         continue;
-    //     }
-    //     System.out.println("Number was " + n2);
-    // } while(true);
-
-    // check if a number is prime or not
-
-    int n3 = sc.nextInt();
-    if (n3 == 2) {
-        System.out.println("N is prime");
-    } else{
-
-    boolean isPrime = true;
-    for(int i5 = 2; i5 <= Math.sqrt(n3); i5++) {
-    if (n3 % i5 == 0) {
-        isPrime = false;
-    }
-    }
+    do {
+        System.out.print("Enter Your Number : ");
+        int n2 = sc.nextInt();
+        if (n2 % 10 == 0) {
+            continue;
+        }
+        System.out.println("Number was " + n2);
+    } while(true);  
     
+     // check if a number is prime or not
 
-    if (isPrime == true) {
-        System.out.println("N is Prime");
-    } else {
-        System.out.println("N is not Prime");
-    }
-} 
+
+//     int pNumber = sc.nextInt();
+//     // int pNumber = 7; 
+//     if (pNumber == 2) {
+//         System.out.println("N is prime");
+//     } else{
+
+//     boolean isPrime = true;
+//     for(int i5 = 2; i5 <= Math.sqrt(pNumber); i5++) {
+//     if (pNumber % i5 == 0) {
+//         isPrime = false;
+//     }
+//     }
+
+
+//     if (isPrime == true) {
+//         System.out.println("N is Prime");
+//     } else {
+//         System.out.println("N is not Prime");
+//     }
+// }
+    
+ 
 }
 }
+
---
+++ java_05_Pattern.java
@@ -0,0 +1,47 @@
+public class java_05_Pattern {
+    public static void main(String[] args) {
+
+        // Star pattern
+
+        for(int line1 = 1; line1 <= 4; line1++) {
+            //  one line
+            for(int line2 = 1; line2 <= line1; line2++) {
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
+
+        //  Inverted star pattern
+
+        int n1 = 4;
+        for(int line3 = 1; line3 <= n1; line3++) {
+            for(int line4 = 1; line4 <= n1-line3+1; line4++) {
+        // System.out.print("* ");
+            }
+            // System.out.println();
+        }
+
+        // Half Pyramid Pattern
+        
+        int n2 = 4;
+        for(int line5 = 1; line5 <=n2; line5++) {
+            for(int number = 1; number <= line5; number++) {
+                // System.out.print(number + " ");
+            }
+            // System.out.println();
+        } 
+
+        //  Character Pattern
+
+        int n3 = 4; 
+        char ch = 'A';
+
+        for(int line6 = 1; line6 <= n3; line6++) {
+           for(int character = 1; character <= line6; character++) {
+            System.out.print(ch + " ");
+            ch++;
+           }
+           System.out.println();
+        }
+    }
+}
