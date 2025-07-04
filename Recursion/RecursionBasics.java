c8d086c82482a53f85b331aa2aa4703cc9369297
Bhawna Pal <bhawnapal2104@gmail.com>
2025-07-04 07:37:44 +0530

    Initial Commit

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -31,10 +31,33 @@ public class RecursionBasics{
         return fn;
    }
 
+//     Print sum of first n natural number
+
+   public static int calcSum(int n) {
+    if(n == 1 ) {
+        return 1;
+    }
+   int Sum1 = calcSum(n-1);
+   int Sn = n + Sum1;
+   return Sn;
+   }
+
+//    Fibonacci number
+    public static int Fibonacci(int n) {
+        if(n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = Fibonacci(n-1);
+        int fnm2 = Fibonacci(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
     public static void main(String args[]) {
-        int n = 5;
+        int n = 10;
         // printDec(n);
         // printInc(n);
-       System.out.println( fact(n));
+    //    System.out.println( fact(n));
+    // System.out.println(calcSum(n));
+    System.out.println(Fibonacci(n));
     }
 }
